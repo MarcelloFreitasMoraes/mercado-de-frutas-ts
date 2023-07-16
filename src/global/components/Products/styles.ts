79ed7191701b2e19577c5fdb98bda8321c0fc39d
Marcelo Moraes <programadormarcelo01@gmail.com>
2023-07-16 20:45:54 -0300

    mobile

--- src/global/components/Products/styles.ts
+++ src/global/components/Products/styles.ts
@@ -3,17 +3,28 @@ import styled from 'styled-components'
 export const Content = styled.div`
     margin: 4% auto;
 `
-export const Grid = styled.div``
+export const Grid = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+
+    @media screen and (max-width: 768px) {
+    justify-content: flex-start;
+  }
+    @media screen and (max-width: 744px) {
+    justify-content: center;
+  }
+`
 
 export const Box = styled.div`
-    margin: 20px 0;
+    margin: 20px 10px;
+    width: 33.33%;
+    max-width: 300px;
     text-align: center;
-    width: 100%;
     background-color: white;
     border-radius: 10px;
     box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.1);
     transition: all 0.3s ease-out;
-
     img {
         margin-bottom: 10px;
     }
@@ -26,6 +37,14 @@ export const Box = styled.div`
         transition: all 0.3s ease-out;
         transform: scale(1.05);
     }
+
+    @media screen and (max-width: 768px) {
+        width: 50%;
+  }
+
+  @media screen and (max-width: 744px) {
+    width: 100%;
+  }
 `
 
 export const Pricing = styled.div`

2ced028bba80db79e6dfd0a21035b1c36ff37a0e
Marcelo Moraes <programadormarcelo01@gmail.com>
2023-07-16 17:57:50 -0300

    login

--- src/global/components/Modal/styles.ts
+++ src/global/components/Modal/styles.ts
@@ -8,7 +8,7 @@ export const Login = styled.div`
     display: flex;
     flex-direction: column;
     position: fixed;
-    right: -10%;
+    right: 5%;
     z-index: 10;
     box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.1);
 `
